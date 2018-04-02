4bfc61f97075950fb74f2ffa1e13e646b0924434
Vildan <vildantursic@hotmail.com>
2018-04-02 20:51:57 +0200

    modeling with faker

--- src/graph/index.root.ts
+++ src/graph/index.root.ts
@@ -1,5 +1,20 @@
-export const root = {
-    hello: () => {
-        return 'Hello world!';
+import faker from 'faker';
+
+const developer = {
+    name: () => faker.name.firstName(),
+    surname: () => faker.name.lastName(),
+    address: () => faker.address.city(),
+};
+
+const company = {
+    name: () => faker.name.firstName(),
+    developers: (args) => {
+        return Array(args.size).fill(developer);
     }
+};
+
+export const root = {
+    companies: (args) => {
+        return Array(args.size).fill(company);
+    },
 };
--- src/graph/index.schema.ts
+++ src/graph/index.schema.ts
@@ -2,6 +2,15 @@ import { buildSchema } from 'graphql';
 
 export const schema = buildSchema(`
   type Query {
-    hello: String
+    companies(size: Int!): [Company],
+  }
+  type Developer {
+    name: String,
+    surname: String,
+    address: String,
+  }
+  type Company {
+    name: String,
+    developers(size: Int!): [Developer]
   }
 `);
