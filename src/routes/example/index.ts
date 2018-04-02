c2418470ec75eb0bcd9c30b1ee4b2df6594e1e57
Vildan <vildantursic@hotmail.com>
2018-04-02 20:59:33 +0200

    adding models

--- src/routes/example/index.ts
+++ src/routes/example/index.ts
@@ -1,4 +1,42 @@
 import express from 'express';
 const router = express.Router();
 
+import Example from './models/example/index.model';
+
+/**
+ *
+ */
+router.get('/example', (req: Request, res: Response) => {
+    res.json({
+        message: 'W-API example GET'
+    });
+});
+
+/**
+ *
+ */
+router.post('/example', (req: Request, res: Response) => {
+    res.json({
+        message: 'W-API example POST'
+    });
+});
+
+/**
+ *
+ */
+router.put('/example/:id', (req: Request, res: Response) => {
+    res.json({
+        message: 'W-API example PUT'
+    });
+});
+
+/**
+ *
+ */
+router.delete('/example/:id', (req: Request, res: Response) => {
+    res.json({
+        message: 'W-API example DELETE'
+    });
+});
+
 module.exports = router;
